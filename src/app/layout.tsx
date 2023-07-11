5a266cbfd04265405b68761bddb8560cf7254e55
dokar3 <68095777+dokar3@users.noreply.github.com>
2023-07-11 13:02:19 +0800

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Jost } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "UpNext GPT",
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={jost.className}>{children}</body>
+      <body className={jost.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
