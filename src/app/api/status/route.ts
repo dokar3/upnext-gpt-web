1c5dc4994724c4b0071f677030834ffc88390396
dokar3 <68095777+dokar3@users.noreply.github.com>
2024-04-08 15:23:51 +0800

    Fix api status response

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,7 +1,12 @@
 import { NextResponse } from "next/server";
 
+const model =
+  process.env.MODEL_PROVIDER === "google"
+    ? process.env.GOOGLE_GEN_AI_MODEL
+    : process.env.OPENAI_MODEL;
+
 const statusMessage = `
-Using '${process.env.OPENAI_MODEL}',
+Using '${model}',
 ${process.env.MAX_REQUESTS_PER_MINUTE_PER_IP} requests / minute, ${process.env.MAX_REQUESTS_PER_DAY_PER_IP} requests / day.
 `.trim();
 
